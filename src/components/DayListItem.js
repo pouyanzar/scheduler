3a05261c14e7228880d9f32721eb9223433d4940
Pouyan Zarbafian <pouyan.zarbafian@hotmail.com>
2021-09-01 15:41:13 -0400

    add styling to DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import 'components/DayListItem.scss';
+var classNames = require('classnames');
 
 export default function DayListItem(props) {
+
   const {name, spots, setDay} = props;
+  const dayClass = classNames('day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': props.spots === 0})
   return (
-    <li onClick={() => setDay(name)}>
+    <li className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{spots}</h3>
     </li>
