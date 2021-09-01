693c6fffc16ecc77123ac9e8f4bb228e6faf37f2
Pouyan Zarbafian <pouyan.zarbafian@hotmail.com>
2021-09-01 17:20:39 -0400

    add values from props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,12 +1,18 @@
+import React from 'react';
+var classNames = require('classnames');
+
 export default function InterviewerListItem(props) {
+
+  const {name, avatar, selected} = props;
+  const interviewerClass = classNames("interviewers__item", {"interviewers__item--selected":selected});
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={avatar}
+        alt={name}
       />
-      Sylvia Palmer
+      {name}
     </li>
   );
 }
