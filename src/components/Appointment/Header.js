a362b6935cc868e7d45a10ce7b7cdad91b7352c5
Pouyan Zarbafian <pouyan.zarbafian@hotmail.com>
2021-09-01 19:52:24 -0400

    pass time as props into Header

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 import "components/Appointment/styles.scss";
 
 export default function Header() {
+  const {time} = props;
   return (
     <header className="appointment__time">
-      <h4 className="text--semi-bold">12pm</h4>
+      <h4 className="text--semi-bold">{time}</h4>
       <hr className="appointment__separator" />
     </header>
   )
