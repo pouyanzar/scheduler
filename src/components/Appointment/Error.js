e89d34242659de6f7147e31699f57f20911c82f3
Pouyan Zarbafian <pouyan.zarbafian@hotmail.com>
2021-09-01 21:13:20 -0400

    pass values as props

--- src/components/Appointment/Error.js
+++ src/components/Appointment/Error.js
@@ -2,11 +2,13 @@ import React from 'react';
 import "components/Appointment/styles.scss";
 
 export default function Error() {
+
+  const {message} = props;
   return (
     <main className="appointment__card appointment__card--error">
       <section className="appointment__error-message">
         <h1 className="text--semi-bold">Error</h1>
-        <h3 className="text--light">Could not delete appointment</h3>
+        <h3 className="text--light">{message}</h3>
       </section>
       <img
         className="appointment__error-close"
