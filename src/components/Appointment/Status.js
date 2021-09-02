e2d2a69e7bf0100fefa1355b1f26c4f39f6b94e7
Pouyan Zarbafian <pouyan.zarbafian@hotmail.com>
2021-09-01 21:06:19 -0400

    pass values as props

--- src/components/Appointment/Status.js
+++ src/components/Appointment/Status.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "components/Appointment/styles.scss";
 
 export default function Status() {
+  const {message} = props;
   return (
     <main className="appointment__card appointment__card--status">
       <img
@@ -9,7 +10,7 @@ export default function Status() {
         src="images/status.png"
         alt="Loading"
       />
-      <h1 className="text--semi-bold">Deleting</h1>
+      <h1 className="text--semi-bold">{message}</h1>
     </main>
   )
 }
