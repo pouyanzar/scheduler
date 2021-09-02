9fee41ee30388f96958be54a8a838471b11e3062
Pouyan Zarbafian <pouyan.zarbafian@hotmail.com>
2021-09-01 20:47:33 -0400

    pass values as props to Show

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -2,13 +2,14 @@ import React from 'react';
 import "components/Appointment/styles.scss";
 
 export default function Show(props) {
+  const {student, name, onDelete, onEdit} = props;
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
-        <h2 className="text--regular">Lydia Miller-Jones</h2>
+        <h2 className="text--regular">{student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">Sylvia Palmer</h3>
+          <h3 className="text--regular">{name}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
@@ -17,11 +18,13 @@ export default function Show(props) {
             className="appointment__actions-button"
             src="images/edit.png"
             alt="Edit"
+            onClick={onEdit}
           />
           <img
             className="appointment__actions-button"
             src="images/trash.png"
             alt="Delete"
+            onClick={onDelete}
           />
         </section>
       </section>
